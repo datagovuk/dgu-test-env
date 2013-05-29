e9d680afa9fb3d9db65fa71cab9eecf31f2388da
Ross Jones <ross@servercode.co.uk>
2013-05-29 10:17:50 +0100

    Removed DB setup steps

--- provision_jenkins.sh
+++ provision_jenkins.sh
@@ -15,40 +15,6 @@ CKAN_INI="pyenv/src/ckan/development.ini"
 set -e 
 set -x
 
-# Postgres and Postgis
-# ====================
-postgres_user_exists() {
-  USER=$1
-  RESULT=`sudo -u postgres psql -c "select 1 from pg_roles where rolname='$USER';" | wc -l`
-  # Query outputs 5 lines if user exists. 4 otherwise.
-  echo $RESULT -eq 5 
-}
-
-postgres_database_ready() {
-  DATABASE=$1
-  sudo -u postgres psql -d $DATABASE -c "select postgis_lib_version();" >/dev/null 2>&1
-  echo $? -eq 0
-}
-
-if [ ! $(postgres_user_exists $USER) ] ; then
-  sudo -u postgres createuser --superuser $USER 2>/dev/null
-fi
-
-if [ ! $(postgres_database_ready template_postgis) ] ; then
-  echo 'Creating template PostGIS database...'
-  createdb template_postgis
-  #sudo -u postgres createlang -d template_postgis plpgsql;
-  psql -d template_postgis -c "CREATE EXTENSION hstore;"
-  psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-  psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
-  psql -d template_postgis -c "select postgis_lib_version();" 
-  psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
-  psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
-  psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
-else
-  echo 'Skipping creation of template PostGIS database.'
-fi
-
 # CKAN/Python setup
 if [ ! -e pyenv ]
 then
@@ -127,12 +93,3 @@ pip install PasteScript==1.7.5
 pip install -r pip-dependancies.txt
 
 #dgu-test-env/scripts/install_ckan_ini.sh $CKAN_INI
-
-if [ ! $(postgres_user_exists ckanuser) ] ; then
-  psql -d postgres -c "create user ckanuser nocreatedb nocreateuser password '`cat .pgpasswd`';"
-fi
-if [ ! $(postgres_database_ready ckan) ] ; then 
-  createdb -O ckanuser ckan --template template_postgis
-fi
-
-
